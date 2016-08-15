ec85593db1b7c951d60e055c46ad6d75b91c0c5c
QuangLoc <kieuquangloc147@gmail.com>
2016-08-15 10:40:26 +0700

    init project exmaple

---
+++ LOC/ExampleCode/LOcCreate/LOcCreate/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LOcCreate
+//
+//  Created by QuangLoc on 8/15/16.
+//  Copyright © 2016 QuangLoc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
