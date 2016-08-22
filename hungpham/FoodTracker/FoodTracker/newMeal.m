789e83499b25249e2ccd733f1bec7881fcc38d03
hungpham22 <hungphamuit@gmail.com>
2016-08-22 23:57:10 +0700

    remove Ref, use files, and ignore Pod file, just keep pod ]config target file

---
+++ hungpham/FoodTracker/FoodTracker/newMeal.m
@@ -0,0 +1,17 @@
+//
+//  newMeal.m
+//  FoodTracker
+//
+//  Created by Hungpv on 8/20/16.
+//  Copyright © 2016 hungpham. All rights reserved.
+//
+
+#import "newMeal.h"
+
+@implementation newMeal
+
+- (void)setDataMeal:(NSDictionary *)dicData newMeal:(Meal *)meal {
+    //meal.mealName = [dicData @"title"];
+}
+
+@end
