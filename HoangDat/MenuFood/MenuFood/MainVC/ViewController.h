6786ae78df83be091b84c1e75c127e19a06b0847
Hackintosh-DatSinGum <Hackintosh-DatSinGum@OSXVNs-MacBook-Pro.local>
2016-08-23 14:26:01 +0700

    Dat push code

---
+++ HoangDat/MenuFood/MenuFood/MainVC/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  MenuFood
+//
+//  Created by Hackintosh-DatSinGum on 8/14/16.
+//  Copyright © 2016 AnhSinGum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UISearchBar *search;
+
+@property (weak, nonatomic) IBOutlet UITableView *TBV;
+
+@end
+
