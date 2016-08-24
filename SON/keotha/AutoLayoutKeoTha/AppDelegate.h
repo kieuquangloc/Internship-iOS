1699490de9f53d2dee6aaa6194015431e8d59370
ThanhSon <thanhsoncun@gmail.com>
2016-08-24 20:02:43 +0700

    keotha

---
+++ SON/keotha/AutoLayoutKeoTha/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutKeoTha
+//
+//  Created by ThanhSon on 8/24/16.
+//  Copyright © 2016 ThanhSon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
